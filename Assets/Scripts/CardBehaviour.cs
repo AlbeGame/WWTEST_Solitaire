eabdf5953087642d8724d3e296f2bd711929a3ef
AlbertoDesktop <alberto.paparella.88@gmail.com>
2018-08-03 10:13:57 +0200

    Import degli asset iniziali

---
+++ Assets/Scripts/CardBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WWTEST
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class CardBehaviour : MonoBehaviour
+    {
+        public Sprite Front;
+        public Sprite Back;
+
+        SpriteRenderer spriteRnd;
+        bool frontFaced;
+        
+        // Use this for initialization
+        void Start()
+        {
+            spriteRnd = GetComponent<SpriteRenderer>();
+        }
+
+        public void Flip()
+        {
+            frontFaced = !frontFaced;
+
+        }
+    }
+
+    public struct CardValue
+    {
+        public byte Number;
+        public byte Seed; 
+    }
+}
