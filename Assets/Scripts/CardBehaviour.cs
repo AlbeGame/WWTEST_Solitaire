2c60d3e4e4df39df2cc3dc9e340c79a9c56f6bd9
AlbertoDesktop <alberto.paparella.88@gmail.com>
2018-08-03 10:41:11 +0200

    inserito DotWeen, sistema di rotazione delle carte e singleton del GameManager

--- Assets/Scripts/CardBehaviour.cs
+++ Assets/Scripts/CardBehaviour.cs
@@ -1,27 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
+using DG.Tweening;
 
 namespace WWTEST
 {
     [RequireComponent(typeof(BoxCollider2D))]
     public class CardBehaviour : MonoBehaviour
     {
-        public Sprite Front;
-        public Sprite Back;
+        Sprite front { get { return GameManager.I.cardsData.Front; } }
+        Sprite back { get { return GameManager.I.cardsData.Back; } }
 
         SpriteRenderer spriteRnd;
+        BoxCollider2D boxColl;
         bool frontFaced;
         
         // Use this for initialization
         void Start()
         {
             spriteRnd = GetComponent<SpriteRenderer>();
+            boxColl = GetComponent<BoxCollider2D>();
         }
 
+        Tween tweenFlip;
+        float flipTime = 0.1f;
         public void Flip()
         {
             frontFaced = !frontFaced;
+            boxColl.enabled = false;
+            //Tween that manage rotation and sprite change
+            tweenFlip = transform.DORotate(new Vector3(0, 90, 0), flipTime/2).OnComplete(() =>
+            {
+                spriteRnd.sprite = frontFaced ? front : back;
+                transform.DORotate(Vector3.zero, flipTime/2).OnComplete(()=> 
+                {
+                    boxColl.enabled = true;
+                });
+            });
+        }
 
+        private void OnMouseUpAsButton()
+        {
+            Flip();
         }
     }
 
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace WWTEST
+{
+    public class GameManager : MonoBehaviour
+    {
+        public DeckData cardsData;
+        public static GameManager I { get; private set; }
+
+        private void Awake()
+        {
+            if (GameManager.I != null)
+                DestroyImmediate(this.gameObject);
+            else
+                I = this;
+        }
+
+        [System.Serializable]
+        public struct DeckData
+        {
+            public Sprite Back;
+            public Sprite Front;
+        }
+    }
+}
