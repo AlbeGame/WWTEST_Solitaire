88ec9a9b90d0434e61458a8f7f3139a635ef7278
AlbertoDesktop <alberto.paparella.88@gmail.com>
2018-08-03 10:51:33 +0200

    Salvata struttura dati per le carte nel gm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,8 +18,28 @@ namespace WWTEST
         [System.Serializable]
         public struct DeckData
         {
+            [Header ("Base")]
             public Sprite Back;
             public Sprite Front;
+            [Header ("Seeds")]
+            public Sprite Clubs;
+            public Sprite Diamonds;
+            public Sprite Hearts;
+            public Sprite Spades;
+            [Header("Numbers")]
+            public Sprite Ace;
+            public Sprite Two;
+            public Sprite Three;
+            public Sprite Four;
+            public Sprite Five;
+            public Sprite Six;
+            public Sprite Seven;
+            public Sprite Eight;
+            public Sprite Nine;
+            public Sprite Ten;
+            public Sprite Jack;
+            public Sprite Queen;
+            public Sprite King;
         }
     }
 }
